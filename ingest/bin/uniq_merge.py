f0dbcde00c5d62578fcca48da6845b3ae24341bd
j23414 <jennifer.chang.bioinform@gmail.com>
2023-05-07 00:42:27 -0700

    [wip] Merge with cached metadata
    This is a work in progress as a holdover to create the live build, until
    a better solution is found.
    
      nextstrain build . terra/metadata_all.tsv  # visual check, deal with data conflicts
      nextstrain build . terra/metadata_denv1.tsv # split into separate builds

---
+++ ingest/bin/uniq_merge.py
@@ -0,0 +1,160 @@
+#! /usr/bin/env python
+
+"""Harmonize and merge pandas DataTables such that conflicting data is not lost.
+
+Leave one blank line.  The rest of this docstring should contain an
+overall description of the module or program.  Optionally, it may also
+contain a brief description of exported classes and functions and/or usage
+examples.
+
+  Typical usage example:
+
+  one_df = pd.DataFrame(
+    {'strain': ['A', 'B', 'C'],
+     'date': ['2022-01-01', '2022-02-02', '2022-03-03'],
+     'clade': ['alpha', 'beta', 'gamma'],
+    'geo':['iowa', 'washington', np.nan]})
+  one_df['age'] = '-N/A-'
+
+  two_df = pd.DataFrame(
+    {'strain': ['D', 'B', 'C'],
+     'clade': ['delta', 'beta2', 'gamma'],
+    'patient': ['bob', 'marley', 'rick']})
+  two_df['col_date'] = np.nan
+  two_df['group'] = ''
+
+  print(one_df)
+  print(two_df)
+
+  merged_df = merge_two(one_df, two_df)
+  print(merged_df)
+"""
+# ===== Dependencies
+import argparse
+import os
+import sys
+
+import numpy as np
+import pandas as pd
+
+
+# (2) Define command line arguments
+def parse_args():
+    # Main help command
+    parser = argparse.ArgumentParser(
+        description="Harmonize and merge pandas DataTables such that conflicting data is not lost."
+    )
+    # Add first argument
+    parser.add_argument("--cache", help="path to cache of cleaned data.", required=True)
+    parser.add_argument("--new", help="path to new data.", required=True)
+    parser.add_argument(
+        "--cache_delim",
+        default="\t",
+        help="delimiter for cache of cleaned data.",
+        required=False,
+    )
+    parser.add_argument(
+        "--new_delim", default="\t", help="delimiter for new data.", required=False
+    )
+    parser.add_argument(
+        "--outfile",
+        default="merged_cache_new.tsv",
+        help="merged file [default: merged_cache_new.tsv].",
+        required=False,
+    )
+    parser.add_argument(
+        "--outfile_excel",
+        help="if defined, will create an excel (*.xlsx) merged file.",
+        required=False,
+    )
+    parser.add_argument(
+        "--outfile_delim",
+        default="\t",
+        help="delimiter for outfile data.",
+        required=False,
+    )
+    parser.add_argument(
+        "--groupby_col",
+        default="strain",
+        help="group by column name [default 'strain'].",
+        required=False,
+    )
+
+    return parser.parse_args()
+
+
+# ===== Reusable functions
+def _drop_uninformative_cols(df: pd.DataFrame) -> pd.DataFrame:
+    """Drops uninformative columns from a pandas DataFrame for being all empty. Used by merge_two."""
+    return df.replace("", np.nan).replace("-N/A-", np.nan).dropna(how="all", axis=1)
+
+
+def _uniq_merge(x: "pd.Series[str]") -> str:
+    """Merges unique values by group and joins conflicting values in a comma separated list. Used by merge_two."""
+    cx = x.replace("", np.nan).replace("-N/A-", np.nan).replace("?",np.nan).dropna().unique()
+    if len(cx) >= 1:
+        # split substrings by delimiter and flatten list
+        my_list = [i.split(',') for i in cx]
+        flat_list = [item for sublist in my_list for item in sublist]
+        # return unique values joined by delimiter
+        return ",".join(list(set(flat_list)))
+    else:
+        return ""
+
+# Merge and harmonize two datasets, flag conflicts with commas
+def merge_two(
+    df1: pd.DataFrame, df2: pd.DataFrame, groupby_col: str = "strain"
+) -> pd.DataFrame:
+    """Harmonizes and merges two pandas DataFrames.
+
+    Takes two pandas DataFrames through the following 3 steps:
+
+    1. Drops any columns in either which are all NA, "-N/A", or empty strings
+    2. Harmonizes their columns such that columns in the left DataFrame are preferentially listed first
+    3. Combines the DataFrames by group defined in groupby_col such that:
+      * unique values are merged
+      * conflicting values are joined in a comma separated list
+
+    Args:
+      df1:
+        The left hand side (lhs) pandas DataTable, will preferentially decide column order of merged DataTable
+      df2:
+        The right hand side (rhs) pandas DataTable, will be merged with df1 and new columns will be listed later.
+      groupby_col:
+        The id column that is shared by both df1 and df2 to allow for merging and harmonization of datasets
+
+    Returns:
+      A merged and harmonized dataset of containing information from df1 and df2.
+
+    Raises:
+      TBD
+    """
+    # Drop uninformative columns
+    df1 = _drop_uninformative_cols(df1)
+    df2 = _drop_uninformative_cols(df2)
+
+    # Harmonize columns
+    new_col = [x for x in df2.columns.tolist() if x not in set(df1.columns.tolist())]
+    h_df1_df = df1.reindex(df1.columns.tolist() + new_col, axis=1)
+    h_df2_df = df2.reindex(df1.columns.tolist() + new_col, axis=1)
+
+    # Unique and merge conflicting data
+    merged_df = pd.concat([h_df1_df, h_df2_df]).groupby(groupby_col).agg([_uniq_merge])
+    merged_df.columns=[i[0] for i in merged_df.columns]
+    return merged_df
+
+
+def main():
+    args = parse_args()
+
+    old = pd.read_csv(args.cache, sep=args.cache_delim, header=0, dtype=str)
+    new = pd.read_csv(args.new, sep=args.new_delim, header=0, dtype=str)
+
+    merged = merge_two(old, new, groupby_col=args.groupby_col)
+    merged.to_csv(args.outfile, sep=args.outfile_delim)
+    if args.outfile_excel is not None:
+        merged.to_excel(args.outfile_excel)
+
+
+if __name__ == "__main__":
+    main()
---
+++ ingest/workflow/snakemake_rules/merge.smk
@@ -0,0 +1,120 @@
+rule pull_cache:
+  output:
+    cache_metadata = "data/cache_metadata.tsv",
+    cache_sequences = "data/cache_sequences.fasta",
+  shell:
+    """
+    # Pull current metadata from Terra
+    gsutil cp 'gs://fc-29a44672-3ae3-4dc8-8c5c-e0c71c36fda3/A_Pathogen_Dengue/metadata_all.tsv' {output.cache_metadata}
+    gsutil cp 'gs://fc-29a44672-3ae3-4dc8-8c5c-e0c71c36fda3/A_Pathogen_Dengue/sequences_all.fasta' {output.cache_sequences}
+    """
+
+rule merge_sequences:
+    input:
+        cache_sequences = "data/cache_sequences.fasta",
+        sequences = "data/sequences_all.fasta",
+    output:
+        sequences = "terra/sequences_all.fasta",
+    shell:
+        """
+        # Merge new sequences with current sequences
+        cat {input.sequences} {input.cache_sequences} > temp.fasta
+
+        # This is a temporary solution to the problem of duplicate sequences
+        # smof is an external dependency not in Docker, nor is this expected to be a permanent solution
+        smof uniq --first-header temp.fasta > {output.sequences}
+        rm temp.fasta
+        """
+
+rule merge_metadata:
+    input:
+        cache_metadata = "data/cache_metadata.tsv",
+        metadata = "data/metadata_all.tsv",
+    output:
+        metadata = "data/merged_metadata.tsv",
+    shell:
+        """
+        # Merge new metadata with current metadata
+        python bin/uniq_merge.py \
+          --cache {input.cache_metadata} \
+          --new {input.metadata} \
+          --groupby_col accession \
+          --outfile {output.metadata}
+        """
+
+rule fix_titles:
+    """Entrez call, will take a while"""
+    input:
+        metadata = "data/metadata_all.tsv",
+    output:
+        metadata = "data/fixed_metadata_all.tsv"
+    shell:
+        """
+        cat {input.metadata} \
+        | bin/tsv-to-ndjson \
+        | python bin/transform-citations.py \
+          --group-size 200 \
+          --genbank-id-field accession \
+        | python bin/ndjson-to-tsv \
+          --metadata-columns accession journal title \
+          --metadata {output.metadata} \
+          --id-field accession
+        """
+
+rule fix_citations:
+    input:
+        cache_metadata = "data/cache_metadata.tsv",
+        new_metadata = "data/fixed_metadata_all.tsv",
+        metadata = "data/merged_metadata.tsv",
+    output:
+        metadata = "terra/metadata_all.tsv"
+    shell:
+        """
+        tsv-select -H -f accession,journal,title {input.cache_metadata} > data/cache_citations.tsv
+        cat {input.new_metadata} \
+        | tr '\r' '\n' | grep -v "^$" \
+        | tsv-select -H -f accession,journal,title \
+        > data/new_citations.tsv
+
+        tsv-append -H data/cache_citations.tsv data/new_citations.tsv > data/citations.tsv
+
+        cat {input.metadata} \
+        | tsv-select -H -e journal,title \
+        | tsv-join -H \
+        --filter-file citations.tsv \
+        --key-fields accession \
+        --append-fields journal,title \
+        --z \
+        > {output.metadata}
+        """
+          
+rule split_files:
+    input:
+        metadata = "terra/metadata_all.tsv",
+        sequences = "terra/sequences_all.fasta",
+    output:      
+        metadata_denv1 = "terra/metadata_denv1.tsv",
+        sequences_denv1 = "terra/sequences_denv1.fasta",
+        metadata_denv2 = "terra/metadata_denv2.tsv",
+        sequences_denv2 = "terra/sequences_denv2.fasta",
+        metadata_denv3 = "terra/metadata_denv3.tsv",
+        sequences_denv3 = "terra/sequences_denv3.fasta",
+        metadata_denv4 = "terra/metadata_denv4.tsv",
+        sequences_denv4 = "terra/sequences_denv4.fasta",
+    shell:
+        """
+        tsv-filter -H --str-eq serotype:denv1 {input.metadata} > {output.metadata_denv1}
+        tsv-filter -H --str-eq serotype:denv2 {input.metadata} > {output.metadata_denv2}
+        tsv-filter -H --str-eq serotype:denv3 {input.metadata} > {output.metadata_denv3}
+        tsv-filter -H --str-eq serotype:denv4 {input.metadata} > {output.metadata_denv4}
+        
+        tsv-select -H -f accession {output.metadata_denv1} | grep -v "accession" > terra/denv1.ids
+        tsv-select -H -f accession {output.metadata_denv2} | grep -v "accession" > terra/denv2.ids
+        tsv-select -H -f accession {output.metadata_denv3} | grep -v "accession" > terra/denv3.ids
+        tsv-select -H -f accession {output.metadata_denv4} | grep -v "accession" > terra/denv4.ids
+        
+        smof grep -f terra/denv1.ids {input.sequences} > {output.sequences_denv1}
+        smof grep -f terra/denv2.ids {input.sequences} > {output.sequences_denv2}
+        smof grep -f terra/denv3.ids {input.sequences} > {output.sequences_denv3}
+        smof grep -f terra/denv4.ids {input.sequences} > {output.sequences_denv4}
+        """
